f696d49322aac7310cac2d47f7124ff1bc1151d8
MateoJJaramilloS <mateojaramillosalinas@gmail.com>
2026-01-08 12:51:15 -0500

    Corte del taller

--- ejercicios/Problemas/src/paquete02/Problema2.java
+++ ejercicios/Problemas/src/paquete02/Problema2.java
@@ -19,20 +19,52 @@ public class Problema2 {
     public static void main(String[] args) {
         // TODO code application logic here
         String[] viviendas = obtenerListadoViviendas();
-        int [][] consumos = obtenerMatrizConsumos(viviendas);
-        int [] sumaAnual = calcularSumaAnual(consumos);
+        int[][] consumos = obtenerMatrizConsumos(viviendas);
+        int[] sumaAnual = calcularSumaAnual(consumos);
         generarReporte(viviendas, sumaAnual);
     }
-    public static String obtenerListadoViviendas(){
-        Scanner entrada = new Scanner(System.in);
-        String [] vivienda =  new String[10];
-        int o = 1;
 
-        for (int i=0; i < vivienda.length; i++){
-            System.out.println("Ingrese identificacion o nombre de casa"+ o);
-            vivienda[i]=entrada.nextLine();
-            o = 1+1;
+    public static String[] obtenerListadoViviendas() {
+        Scanner entrada = new Scanner(System.in);
+        String[] viviendas = new String[10];
+        for (int i = 0; i < viviendas.length; i++) {
+            System.out.println("Ingrese identificacion o nombre de casa" + (i + 1));
+            viviendas[i] = entrada.nextLine();
         }
+        return viviendas;
     }
-    
+
+    public static int[][] obtenerMatrizConsumos(String[] viviendas) {
+        Scanner entrada = new Scanner(System.in);
+        int[][] consumos = new int[viviendas.length][12];
+        for (int i = 0; i < viviendas.length; i++) {
+            System.out.println("Consumos de " + viviendas[i]);
+            for (int j = 0; j < 12; j++) {
+                System.out.print("Mes " + (j + 1) + ": ");
+                consumos[i][j] = entrada.nextInt();
+
+            }
+        }
+        return consumos;
+    }
+
+    public static int[] calcularSumaAnual(int[][] consumos) {
+        int[] sumaAnual = new int[consumos.length];
+        for (int i = 0; i < consumos.length; i++) {
+            int suma = 0;
+            for (int j = 0; j < consumos[i].length; j++) {
+                suma = suma + consumos[i][j];
+            }
+            sumaAnual[i] = suma;
+        }
+        return sumaAnual;
+    }
+
+    public static void generarReporte(String[] viviendas, int[] sumaAnual) {
+        String reporte = "\n---REPORTE DE CONSUMO ANUAL---\n";
+        for (int i = 0; i < viviendas.length; i++) {
+            reporte += String.format("Vivienda %s: %d kWh\n", viviendas[i], sumaAnual[i]);
+        }
+        System.out.println(reporte);
+    }
 }
--- ejercicios/Problemas/src/paquete03/Problema3.java
+++ ejercicios/Problemas/src/paquete03/Problema3.java
@@ -5,6 +5,8 @@
  */
 package paquete03;
 
+import java.util.Scanner;
+
 /**
  *
  * @author reroes
@@ -16,6 +18,49 @@ public class Problema3 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner entrada = new Scanner(System.in);
+        int opcion = 0;
+
+        System.out.println("Escoja que area quiere sacar\n1: Área Cuadrado\n2: Área Triángulo\n3: Área Rectángulo\nIngrese la opción: ");
+        opcion = entrada.nextInt();
+        do {
+            if (opcion == 1) {
+                obtenerAreaCuadrado(); 
+            } else {
+                if (opcion == 2) {
+                    obtenerAreaTriangulo();
+                } else {
+                    if (opcion == 3) {
+                        obtenerAreaRectangulo();
+                    }
+                }
+            }
+
+        } while (opcion >= 4);
     }
-    
+    public static void obtenerAreaCuadrado() {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Ingrese un lado de cuadrado:"); 
+        double lado= entrada.nextDouble();
+        double area = lado* lado;
+        System.out.printf("El área del cuadrado es: %s", area);
+    }
+    public static void obtenerAreaTriangulo() {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Ingrese la base del triángulo:"); 
+        double base= entrada.nextDouble();
+        System.out.println("Ingrese la altura del triángulo:"); 
+        double altura= entrada.nextDouble();
+        double area = (base*altura)/2;
+        System.out.printf("El área del triángulo es: %s", area);
+    }
+    public static void obtenerAreaRectangulo() {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Ingrese la base del rectángulo:"); 
+        double base= entrada.nextDouble();
+        System.out.println("Ingrese la altura del rectángulo:"); 
+        double altura= entrada.nextDouble();
+        double area = base*altura;
+        System.out.printf("El área del rectángulo es: %s", area);
+    }
 }
