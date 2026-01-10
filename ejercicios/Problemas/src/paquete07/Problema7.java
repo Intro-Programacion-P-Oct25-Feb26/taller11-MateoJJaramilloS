140bdab822d7610a747329670dc5f6d4a6f1a519
MateoJJaramilloS <mateojaramillosalinas@gmail.com>
2026-01-09 23:08:34 -0500

    Problema 7

--- ejercicios/Problemas/src/paquete07/Problema7.java
+++ ejercicios/Problemas/src/paquete07/Problema7.java
@@ -9,6 +9,35 @@ package paquete07;
  *
  * @author reroes
  */
+import java.util.Scanner;
+
 public class Problema7 {
-    
+
+    public static void main(String[] args) {
+        String[] ciudades = numeroCiudades();
+        imprimirCiudades(ciudades);
+    }
+
+    public static String[] numeroCiudades() {
+        Scanner entrada = new Scanner(System.in);
+        System.out.print("Ingrese el número de ciudades que desea ingresar: ");
+        int ciudades = entrada.nextInt();
+        entrada.nextLine();
+
+        String[] arreglo = new String[ciudades];
+        for (int i = 0; i < arreglo.length; i++) {
+            System.out.print("Ingrese ciudad " + (i + 1) + ": ");
+            arreglo[i] = entrada.nextLine();
+        }
+        return arreglo;
+    }
+
+    public static void imprimirCiudades(String[] arreglo) {
+        System.out.println("\nCiudades con 4 o 5 caracteres:");
+        for (int i = 0; i < arreglo.length; i++) {
+            if (arreglo[i].length() == 4 || arreglo[i].length() == 5) {
+                System.out.println(arreglo[i]);
+            }
+        }
+    }
 }
